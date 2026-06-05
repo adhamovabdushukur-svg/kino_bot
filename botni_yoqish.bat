eb96afde171d76b7069952add38224bd6bc2580e
Adkhamov_l7 <adhamovabdushukur@gmail.com>
2026-06-05 18:09:55 +0500

    Add botni_yoqish.bat

---
+++ botni_yoqish.bat
@@ -0,0 +1,8 @@
+@echo off
+color 0a
+echo ===================================================
+echo               KINO BOT ISHGA TUSHMOQDA...
+echo ===================================================
+echo.
+"%LOCALAPPDATA%\Programs\Python\Python311\python.exe" bot.py
+pause
